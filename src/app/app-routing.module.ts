17961cb90a851f0be2cc430bedaacda958f7112b
LAPTOP-M62VF5F0\Sergio-P <sergiotorrome09@gmail.com>
2020-02-05 17:51:09 +0100

    Arreglo algunos fallos de codigo con respecto al homepage y a la pantalla de login, además añado la pantalla del formulario

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -7,7 +7,11 @@ const routes: Routes = [
   {
     path: 'login-screen',
     loadChildren: () => import('./screens/login-screen/login-screen.module').then( m => m.LoginScreenPageModule)
+  },  {
+    path: 'pregunta1-p2',
+    loadChildren: () => import('./pregunta1-p2/pregunta1-p2.module').then( m => m.Pregunta1P2PageModule)
   },
+
 ];
 
 @NgModule({
---
+++ src/app/pregunta1-p2/pregunta1-p2.page.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-pregunta1-p2',
+  templateUrl: './pregunta1-p2.page.html',
+  styleUrls: ['./pregunta1-p2.page.scss'],
+})
+export class Pregunta1P2Page implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
