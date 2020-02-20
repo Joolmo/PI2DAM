7fe54bfde28c0d159cee5d7dcb6cefc47984ecc4
LAPTOP-M62VF5F0\Sergio-P <sergiotorrome09@gmail.com>
2020-02-20 17:57:25 +0100

    Algunos cambios

--- src/app/screens/pregunta1-p2/pregunta1-p2.page.ts
+++ src/app/screens/pregunta1-p2/pregunta1-p2.page.ts
@@ -1,5 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { MenuController } from '@ionic/angular';
+import { IFormStruct } from 'src/app/interfaces/interfaces';
+
+
 @Component({
   selector: 'app-pregunta1-p2',
   templateUrl: './pregunta1-p2.page.html',
@@ -7,7 +10,28 @@ import { MenuController } from '@ionic/angular';
 })
 export class Pregunta1P2Page implements OnInit {
 
-  constructor(private menuCtrl: MenuController) { }
+  constructor(private menuCtrl: MenuController) {
+    this.struct={
+      "name" : "Joolmo",
+      "teacherId" : "1",
+      "classesIds" : ["1","2","4"],
+      "fields" : [{
+        "label" : "hola",
+        "requiered" : true,
+        "type" : "text",
+      },
+    { "label" : "hola2",
+    "requiered" : true,
+    "type" : "radio",
+    },
+    { "label" : "hola3",
+    "requiered" : true,
+    "type" : "y/n",
+    }]
+
+    }
+   }
+  struct: IFormStruct;
 
   ngOnInit() {
   }
